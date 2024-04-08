812b8b554a1669652d281724b56bfc273f7a237b
Salah <salah.bouheniche@gmail.com>
2024-04-08 18:53:29 +0200

    Add bureaucrat class implementation

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,7 +6,47 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 12:46:46 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/03/23 12:46:47 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/08 18:52:29 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+    : name("Undefined"), grade(150)
+{
+    std::cout << "Bureaucrat constructed\n";
+}
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade)
+    : name(name), grade(grade)
+{
+    std::cout << "Bureaucrat " << this->name << " constructed\n";
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other)
+    : name(other.name), grade(other.grade)
+{
+    std::cout << "Bureaucrat " << this->name << " copied\n";
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
+{
+    this->grade = other.grade;
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Bureaucrat " << name << " destroyed\n";
+}
+
+const std::string Bureaucrat::getName() const
+{
+    return this->name;
+}
+
+int Bureaucrat::getGrade() const
+{
+    return this->grade;
+}
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,7 +6,29 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 12:46:52 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/03/23 12:46:54 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/08 18:52:51 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+
+class Bureaucrat
+{
+    private :
+        const std::string   name;
+        int                 grade;
+    public :
+        Bureaucrat();
+        Bureaucrat(const std::string &name, int grade);
+        Bureaucrat(const Bureaucrat &other);
+        Bureaucrat& operator=(const Bureaucrat &other);
+        ~Bureaucrat();
+        
+        const std::string getName() const;
+        int getGrade() const;
+};
+
+#endif
