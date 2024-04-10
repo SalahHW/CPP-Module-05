cdd08572fa8c235dda3b9b3a0af0a22d46072c6d
Salah <salah.bouheniche@gmail.com>
2024-04-10 17:13:10 +0200

    Add test

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 12:46:57 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/08 18:52:58 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/10 15:33:30 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,15 @@ int main(void)
 {
     Bureaucrat undefined;
     Bureaucrat salah("Salah", 1);
+    try
+    {
+        Bureaucrat joakoeni("Joakoeni", 150);
+        joakoeni.decrementGrade();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
     
     std::cout << "Bureaucrat name : " << undefined.getName() << " have grade "
               << undefined.getGrade() << "\n";
@@ -26,8 +35,14 @@ int main(void)
     undefined = salah;
     std::cout << "Bureaucrat name : " << undefined.getName() << " have grade "
               << undefined.getGrade() << "\n";
-    undefined = undefined;
-    std::cout << "Bureaucrat name : " << undefined.getName() << " have grade "
-              << undefined.getGrade() << "\n";
+
+    try
+    {
+        salah.incrementGrade();
+    }
+    catch (std::exception & e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
     return (0);
 }
