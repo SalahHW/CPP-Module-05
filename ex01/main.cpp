820910363cbabff8cc3d4e05691291f2e2a4d477
Salah <salah.bouheniche@gmail.com>
2024-04-15 00:53:06 +0200

    Change tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 12:46:57 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/12 01:46:49 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/12 22:35:07 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,12 @@ int main(void)
     // Bureaucrat undefined;
     Bureaucrat salah("Salah", 1);
     Bureaucrat joakoeni("Joakoeni", 150);
-    Form s12("12S", 2, 1);
-
-    std::cout << salah;
-    std::cout << joakoeni;
 
     try
     {
-        joakoeni.incrementGrade();
-        std::cout << joakoeni;
-        salah.incrementGrade();
-        std::cout << salah;
+        Form s12("12S", 51, 151);
     }
-    catch (std::exception & e)
+    catch (std::exception &e)
     {
         std::cerr << e.what() << std::endl;
     }
