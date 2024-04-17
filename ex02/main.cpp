e3648ad436e9d393b5bf93a0104c039066b0263a
Salah <salah.bouheniche@gmail.com>
2024-04-17 04:22:21 +0200

    Include class to implement tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,18 +6,17 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 12:46:57 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/17 03:51:11 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/17 04:21:03 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "class/AForm/AForm.hpp"
 #include "class/AForm/ShrubberyCreationForm/ShrubberyCreationForm.hpp"
+#include "class/AForm/RobotomyRequestForm/RobotomyRequestForm.hpp"
+#include "class/AForm/PresidentialPardonForm/PresidentialPardonForm.hpp"
 #include "class/Bureaucrat/Bureaucrat.hpp"
 
 int main(void)
 {
-  Bureaucrat salah("Salah", 15);
-  ShrubberyCreationForm("home");
-
   return (0);
 }
