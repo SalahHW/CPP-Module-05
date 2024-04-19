8b5882d2286cf5ee6cc3a4ac33b9d9ea57c4bb0a
Salah <salah.bouheniche@gmail.com>
2024-04-19 21:43:41 +0200

    Implement Form class

--- ex01/class/Form/Form.hpp
+++ ex01/class/Form/Form.hpp
@@ -6,11 +6,48 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:43:58 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/19 11:08:40 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/19 21:34:04 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+
 class Form
 {
-    
+private:
+    const std::string name;
+    const int requiredGradeToSign;
+    const int requiredGradeToExecute;
+    bool isSigned;
+
+public:
+    Form();
+    ~Form();
+    Form(Form const &);
+    Form &operator=(Form const &);
+
+    Form(std::string const &name, int requiredGradeToSign, int requiredGradeToExecute);
+
+    std::string const& getName() const;
+    int getRequiredGradeToSign() const;
+    int getRequiredGradeToExecute() const;
+    bool getSignStatus() const;
+
+    // Exceptions
+    class GradeTooHighException : public std::exception
+    {
+        virtual const char *what() const throw();
+    };
+    class GradeTooLowException : public std::exception
+    {
+        virtual const char *what() const throw();
+    };
 };
+
+std::ostream &operator<<(std::ostream &stream, Form const& form);
+#endif
