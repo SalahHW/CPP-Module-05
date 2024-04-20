4ed8916b9d00aecb7f617c3ec5589bbcc9b1ff41
Salah <salah.bouheniche@gmail.com>
2024-04-20 23:41:22 +0200

    Add performAction method in AForm class

--- ex02/class/AForm/AForm.cpp
+++ ex02/class/AForm/AForm.cpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:43:44 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/20 23:20:47 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/20 23:37:35 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ const char *AForm::AlreadySignedException::what() const throw()
 }
 const char *AForm::NotSignedException::what() const throw()
 {
-	return ("Form is not signed");
+    return ("Form is not signed");
 }
 
 // Getters
@@ -103,12 +103,13 @@ void AForm::beSigned(Bureaucrat const &bureaucrat)
               << bureaucrat.getName() << " bureaucrat." << std::endl;
 }
 
-void AForm::execute(Bureaucrat const & executor) const
+void AForm::execute(Bureaucrat const &executor) const
 {
-	if (!isSigned)
-		throw NotSignedException();
-	if (executor.getGrade() > requiredGradeToExecute)
-		throw GradeTooLowException();
+    if (!isSigned)
+        throw NotSignedException();
+    if (executor.getGrade() > requiredGradeToExecute)
+        throw GradeTooLowException();
+    performAction();
 }
 
 // Overloads
--- ex02/class/AForm/AForm.hpp
+++ ex02/class/AForm/AForm.hpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:43:58 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/20 23:10:12 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/20 23:25:28 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,7 @@ public:
 	// Functions
 	void beSigned(Bureaucrat const &);
 	void execute(Bureaucrat const &) const;
+	virtual void performAction() const = 0;
 };
 
 // Overloads
