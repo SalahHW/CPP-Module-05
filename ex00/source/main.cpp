f8f338ba150c27eb194ec0c322adadc241b0a083
Salah <salah.bouheniche@gmail.com>
2024-04-20 06:33:23 +0200

    Update test

--- ex00/source/main.cpp
+++ ex00/source/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 12:46:57 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/19 10:30:23 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/20 06:32:01 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,12 @@ int main(void)
         // Bureaucrat wrong_grade2("Grade", -2147483646);
         Bureaucrat salah("Salah", 1);
         Bureaucrat joakoeni("Joakoeni", 150);
-        std::cout << salah;
-        std::cout << joakoeni;
+        std::cout << salah << std::endl;
+        std::cout << joakoeni << std::endl;
         joakoeni.incrementGrade();
-        std::cout << joakoeni;
+        std::cout << joakoeni << std::endl;
         salah.decrementGrade();
-        std::cout << salah;
+        std::cout << salah << std::endl;
     }
     catch (std::exception &e)
     {
