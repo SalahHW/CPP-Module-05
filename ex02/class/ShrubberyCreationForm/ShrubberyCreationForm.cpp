f7536aef6d8d7c66f18d37425dbd82dddbc3ebae
Salah <salah.bouheniche@gmail.com>
2024-04-20 14:30:13 +0200

    Implement ShrubberryCreationForm class

--- ex02/class/ShrubberyCreationForm/ShrubberyCreationForm.cpp
+++ ex02/class/ShrubberyCreationForm/ShrubberyCreationForm.cpp
@@ -6,10 +6,26 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 09:32:26 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/20 09:45:47 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/20 13:52:27 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../AForm/AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
-#include <iostream>
+
+ShrubberyCreationForm::ShrubberyCreationForm()
+    : AForm("ShrubberyCreationForm", 145, 137), target("") {}
+
+ShrubberyCreationForm::~ShrubberyCreationForm() {}
+
+ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm const &other)
+    : AForm(other), target(other.target) {}
+
+ShrubberyCreationForm &ShrubberyCreationForm::operator=(ShrubberyCreationForm const &other)
+{
+    if (this != &other)
+    {
+        AForm::operator=(other);
+        target = other.target;
+    }
+    return (*this);
+}
--- ex02/class/ShrubberyCreationForm/ShrubberyCreationForm.hpp
+++ ex02/class/ShrubberyCreationForm/ShrubberyCreationForm.hpp
@@ -6,13 +6,27 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 09:32:11 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/20 09:33:36 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/20 13:38:29 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHRUBBERY_CREATION_FORM_HPP
 #define SHRUBBERY_CREATION_FORM_HPP
 
+#include "../AForm/AForm.hpp"
+#include <iostream>
 
+
+class ShrubberyCreationForm : public AForm
+{
+private:
+    std::string target;
+
+public:
+    ShrubberyCreationForm();
+    ~ShrubberyCreationForm();
+    ShrubberyCreationForm(ShrubberyCreationForm const &);
+    ShrubberyCreationForm &operator=(ShrubberyCreationForm const &);
+};
 
 #endif
