15fcd89c2f94bf8305b1c7e09da662124c8211c0
Salah <salah.bouheniche@gmail.com>
2024-04-20 14:30:35 +0200

    Implement RobotomyRequestFormClass

--- ex02/class/RobotomyRequestForm/RobotomyRequestForm.cpp
+++ ex02/class/RobotomyRequestForm/RobotomyRequestForm.cpp
@@ -6,10 +6,26 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 09:37:08 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/20 09:45:44 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/20 13:57:29 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../AForm/AForm.hpp"
 #include "RobotomyRequestForm.hpp"
-#include <iostream>
+
+RobotomyRequestForm::RobotomyRequestForm()
+    : AForm("RobotomyRequestForm", 72, 45), target("Undefined") {}
+
+RobotomyRequestForm::~RobotomyRequestForm() {}
+
+RobotomyRequestForm::RobotomyRequestForm(RobotomyRequestForm const &other)
+    : AForm(other), target("Undefined") {}
+
+RobotomyRequestForm &RobotomyRequestForm::operator=(RobotomyRequestForm const &other)
+{
+    if (this != &other)
+    {
+        AForm::operator=(other);
+        target = other.target;
+    }
+    return (*this);
+}
--- ex02/class/RobotomyRequestForm/RobotomyRequestForm.hpp
+++ ex02/class/RobotomyRequestForm/RobotomyRequestForm.hpp
@@ -6,13 +6,26 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 09:35:28 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/20 09:37:06 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/20 13:49:37 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ROBOTOMY_REQUEST_FORM
 #define ROBOTOMY_REQUEST_FORM
 
+#include "../AForm/AForm.hpp"
+#include <iostream>
 
+class RobotomyRequestForm : public AForm
+{
+private:
+    std::string target;
+
+public:
+    RobotomyRequestForm();
+    ~RobotomyRequestForm();
+    RobotomyRequestForm(RobotomyRequestForm const &);
+    RobotomyRequestForm &operator=(RobotomyRequestForm const &);
+};
 
 #endif
