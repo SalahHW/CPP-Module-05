e8bbf41c2df6e57a45840a9fd52abe71e3ce5840
Salah <salah.bouheniche@gmail.com>
2024-04-20 07:10:50 +0200

    Add tests

--- ex01/source/main.cpp
+++ ex01/source/main.cpp
@@ -6,11 +6,12 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 12:46:57 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/20 06:32:01 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/20 07:09:45 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../class/Bureaucrat/Bureaucrat.hpp"
+#include "../class/Form/Form.hpp"
 
 int main(void)
 {
@@ -22,8 +23,12 @@ int main(void)
         // Bureaucrat wrong_grade2("Grade", -2147483646);
         Bureaucrat salah("Salah", 1);
         Bureaucrat joakoeni("Joakoeni", 150);
+        // Form wrongForm("Wrong", 134334, -23423);
+        Form foo("Foo", 15, 42);
         std::cout << salah << std::endl;
         std::cout << joakoeni << std::endl;
+        std::cout << foo << std::endl;
+        foo.beSigned(joakoeni);
         joakoeni.incrementGrade();
         std::cout << joakoeni << std::endl;
         salah.decrementGrade();
