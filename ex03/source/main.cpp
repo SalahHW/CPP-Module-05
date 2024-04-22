ae7a6bf1cca9b158a8b91dc46edd99f424781306
Salah <salah.bouheniche@gmail.com>
2024-04-22 17:10:13 +0200

    Update tests

--- ex03/source/main.cpp
+++ ex03/source/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 12:46:57 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/21 14:25:15 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/22 17:04:18 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "../class/ShrubberyCreationForm/ShrubberyCreationForm.hpp"
 #include "../class/RobotomyRequestForm/RobotomyRequestForm.hpp"
 #include "../class/PresidentialPardonForm/PresidentialPardonForm.hpp"
+#include "../class/Intern/Intern.hpp"
 
 int main(void)
 {
@@ -22,17 +23,15 @@ int main(void)
     
     try
     {
+        AForm *basket;
+        Intern richard;
         Bureaucrat salah("Salah", 1);
-        Bureaucrat joakoeni("Joakoeni", 150);
-        ShrubberyCreationForm shrubbery("home");
-        RobotomyRequestForm robotomy("Richard");
-        PresidentialPardonForm pardon("Richard");
-        joakoeni.signForm(shrubbery);
-        joakoeni.executeForm(shrubbery);
-        salah.signForm(shrubbery);
-        joakoeni.executeForm(shrubbery);
-        salah.executeForm(shrubbery);
-        salah.executeForm(pardon);
+
+        basket = richard.makeForm("ShrubberyCreationForm", "Desktop");
+        std::cout << *basket << std::endl;
+        // salah.signForm(*basket);
+        salah.executeForm(*basket);
+        delete basket;
     }
     catch (std::exception &e)
     {
